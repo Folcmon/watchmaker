24380cf1dfb8f3be0591577f1c9071d2e2ef0649
Kamil Kosakowski <kamik870310@gmail.com>
2023-03-13 20:37:15 +0100

    add Vat Rate crud

---
+++ src/Controller/VatRateController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\VatRate;
+use App\Form\VatRateType;
+use App\Repository\VatRateRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/vat/rate')]
+class VatRateController extends AbstractController
+{
+    #[Route('/', name: 'app_vat_rate_index', methods: ['GET'])]
+    public function index(VatRateRepository $vatRateRepository): Response
+    {
+        return $this->render('vat_rate/index.html.twig', [
+            'vat_rates' => $vatRateRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_vat_rate_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, VatRateRepository $vatRateRepository): Response
+    {
+        $vatRate = new VatRate();
+        $form = $this->createForm(VatRateType::class, $vatRate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $vatRateRepository->save($vatRate, true);
+
+            return $this->redirectToRoute('app_vat_rate_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('vat_rate/new.html.twig', [
+            'vat_rate' => $vatRate,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_vat_rate_show', methods: ['GET'])]
+    public function show(VatRate $vatRate): Response
+    {
+        return $this->render('vat_rate/show.html.twig', [
+            'vat_rate' => $vatRate,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_vat_rate_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, VatRate $vatRate, VatRateRepository $vatRateRepository): Response
+    {
+        $form = $this->createForm(VatRateType::class, $vatRate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $vatRateRepository->save($vatRate, true);
+
+            return $this->redirectToRoute('app_vat_rate_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('vat_rate/edit.html.twig', [
+            'vat_rate' => $vatRate,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_vat_rate_delete', methods: ['POST'])]
+    public function delete(Request $request, VatRate $vatRate, VatRateRepository $vatRateRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$vatRate->getId(), $request->request->get('_token'))) {
+            $vatRateRepository->remove($vatRate, true);
+        }
+
+        return $this->redirectToRoute('app_vat_rate_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/VatRate.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VatRateRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+/**
+ * Describie available VAT rates in all system 23% == vatValue 23
+ */
+#[ORM\Entity(repositoryClass: VatRateRepository::class)]
+class VatRate
+{
+    use TimestampableEntity;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $rateName = null;
+
+    #[ORM\Column]
+    private ?int $rateValue = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRateName(): ?string
+    {
+        return $this->rateName;
+    }
+
+    public function setRateName(string $rateName): self
+    {
+        $this->rateName = $rateName;
+
+        return $this;
+    }
+
+    public function getRateValue(): ?int
+    {
+        return $this->rateValue;
+    }
+
+    public function setRateValue(int $rateValue): self
+    {
+        $this->rateValue = $rateValue;
+
+        return $this;
+    }
+}

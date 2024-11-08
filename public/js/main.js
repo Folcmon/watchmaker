b93a2b2b515683319f108d8dd56ac0e344ff3018
Kamil Kosakowski <kamilk870310@gmail.com>
2024-11-08 10:11:25 +0100

    Added select2 initialize function to be global available

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
+function initializeSelect2(selector) {
+    $(selector).select2({
+        width: '100%',
+    });
+}
+
 $(document).ready(function () {
     $('.select2').select2({
         width: '100%',

d5b021ffc088a8af4ae1eee42718f556f67b65e9
Kamil Kosakowski <kamilk870310@gmail.com>
2024-11-13 10:04:37 +0100

    Added base login test

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function testLogin()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Please sign in');
+    }
+
+}

91555a2bc7e342cabf0a4d8a10255b967cdcc7c2
Kamil Kosakowski <kamilk870310@gmail.com>
2024-12-17 19:33:49 +0100

    Removed require description in order

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -23,7 +23,7 @@ class Order implements Loggable
     #[ORM\Column(type: 'integer')]
     private ?int $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
     #[Gedmo\Versioned]
     private ?string $description;
 

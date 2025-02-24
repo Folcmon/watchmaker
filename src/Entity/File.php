4b519627ffab4356d6343acf928203af66fc7966
Kamil Kosakowski <kamilk870310@gmail.com>
2025-02-24 10:07:55 +0100

    Add file entity

---
+++ src/Entity/File.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+use DateTimeInterface;
+
+#[ORM\Entity(repositoryClass: "App\Repository\FileRepository")]
+#[ORM\HasLifecycleCallbacks]
+class File
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private ?int $id = null;
+
+    #[ORM\Column(type: "uuid", unique: true)]
+    private Uuid $uuid;
+
+    #[ORM\Column(type: "string", length: 255)]
+    private string $path;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private ?string $originalFilename = null;
+
+    #[ORM\Column(type: "datetime")]
+    private DateTimeInterface $createdAt;
+
+    public function __construct(string $originalFilename, string $extension)
+    {
+        $this->uuid = Uuid::v4();
+        $this->originalFilename = $originalFilename;
+        $this->createdAt = new \DateTime();
+
+        // Tworzymy dynamiczną ścieżkę katalogu na podstawie daty
+        $this->path = sprintf('%s/%s/%s/%s.%s',
+            $this->createdAt->format('Y'),
+            $this->createdAt->format('m'),
+            $this->createdAt->format('d'),
+            $this->uuid->toRfc4122(),
+            $extension
+        );
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): Uuid
+    {
+        return $this->uuid;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function getOriginalFilename(): ?string
+    {
+        return $this->originalFilename;
+    }
+
+    public function getCreatedAt(): DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    #[ORM\PostRemove]
+    public function removeFile(): void
+    {
+        $filePath = __DIR__ . '/../../public/uploads/' . $this->path;
+
+        if (file_exists($filePath)) {
+            unlink($filePath);
+        }
+    }
+}
